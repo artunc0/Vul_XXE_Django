3a6fced34b22da3e44138c2a7f4cbd850d220681
artunc <artunc@sabanciuniv.edu>
2021-12-03 03:27:16 +0300

    simple page finished

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse 
 
+from .models import Book
+
+# Create your views here.
+
 def index(request):
     return HttpResponse("Hello, world!")
-# Create your views here.
+
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'book_details.html', {'book':book})
+    #return HttpResponse(f"Book: {book.title}, published on {book.pub_date}")
+
